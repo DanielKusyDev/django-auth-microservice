26ae15f2b460a8f0f87f6f71215fdfae900adbe8
DanielKusyDev <daniel.kusy@live.umcs.edu.pl>
2020-08-31 17:06:05 +0200

    #4 Create unit tests placeholders.

---
+++ common/tests/test_views.py
@@ -0,0 +1,22 @@
+def test_permission_required_mixin_dict_perms():
+    pass
+
+
+def test_permission_required_mixin():
+    pass
+
+
+def test_base_api_view_responses():
+    pass
+
+
+def test_base_api_view_permission_checking():
+    pass
+
+
+def test_base_viewset_dict_perms():
+    pass
+
+
+def test_model_viewset_mixins():
+    pass
