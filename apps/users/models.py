1b8aecf65668819c14a5e3dd778bb1176cade69f
DanielKusyDev <daniel.kusy@live.umcs.edu.pl>
2020-09-05 10:06:22 +0200

    Add User model along with User Manager with method to creating staff users, user with random password and getting users with given status.

---
+++ apps/users/models.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
+from django.db import models
+
+
+class UserManager(DjangoUserManager):
+    use_in_migrations = True
+
+    def create_user_with_random_password(self, username, email, **kwargs):
+        kwargs.setdefault('is_staff', False)
+        kwargs.setdefault('is_superuser', False)
+        if "password" in kwargs.keys():
+            del kwargs["password"]
+        password = self.make_random_password()
+        return self._create_user(username=username, email=email, password=password, **kwargs)
+
+    def create_staff(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', False)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Staff must have is_staff=True.')
+        if extra_fields.get('is_superuser') is True:
+            raise ValueError('Superuser must NOT have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
+
+    def staff(self):
+        return self.filter(is_staff=True)
+
+    def superusers(self):
+        return self.filter(is_superuser=True)
+
+    def non_staff(self):
+        return self.exclude(is_staff=True).exclude(is_superuser=True)
+
+    def active_only(self):
+        return self.filter(is_active=True)
+
+    def not_active_only(self):
+        return self.filter(is_active=False)
+
+
+class User(AbstractUser):
+    suspended_to = models.DateTimeField(verbose_name=_("suspended to"), null=True, blank=True)
+    objects = UserManager()
+
+    @property
+    def time_since_login(self):
+        return 0
