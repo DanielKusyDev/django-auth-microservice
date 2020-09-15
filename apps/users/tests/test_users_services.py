3d4e079df56086f931f3b45bc6e459d6f6fab774
Daniel Kusy <daniel.kusy@akanza.pl>
2020-09-15 09:20:00 +0200

    Issue #8: Test if token is present in sent reset password message.

--- apps/users/tests/test_users_services.py
+++ apps/users/tests/test_users_services.py
@@ -1,13 +1,16 @@
 import pytest
 from django.core import mail
 from django.test import override_settings, RequestFactory
+from djmail.models import Message
 from rest_framework.views import APIView
 
 from apps.users import services
 
 
 @pytest.mark.django_db
-@override_settings(EMAIL_BACKEND='djmail.backends.default.EmailBackend')
+@override_settings(
+    EMAIL_BACKEND='djmail.backends.default.EmailBackend',
+    DJMAIL_REAL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
 def test_sending_reset_password_email(mocker):
     service = services.ResetPasswordService()
     view = APIView.as_view()
@@ -18,4 +21,4 @@ def test_sending_reset_password_email(mocker):
     token.user.email = 'email@test.com'
     service.send_email_on_token_creation(None, view, token)
     assert len(mail.outbox) == 1
-
+    assert token.key in Message.objects.first().body_html
