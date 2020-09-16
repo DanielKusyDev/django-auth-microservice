881e79c2fd8e2ba542c31476e34f0dbbfbe50c28
Daniel Kusy <daniel.kusy@akanza.pl>
2020-09-16 09:19:14 +0200

    HOTFIX: Add corsheaders support.

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'djmail',
     'django_rest_passwordreset',
+    'corsheaders',
 
     # Internal apps
     'apps.users',
@@ -40,6 +41,7 @@ TEMPLATES = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,13 +70,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Lang & Time zone
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+# Statics
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8080'
+]
 
 # memcached
 CACHES = {
