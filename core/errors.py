eebd5bc43451f9a88607d43ece7c399ae2caa922
DanielKusyDev <daniel.kusy@live.umcs.edu.pl>
2020-10-17 15:16:40 +0200

    Handle invalid token error;

--- core/errors.py
+++ core/errors.py
@@ -1,10 +1,14 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework.views import exception_handler
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 
 def drf_exception_handler(exc, context):
     if not isinstance(exc, ValidationError):
-        exc_detail = {'error': exc}
+        if isinstance(exc, InvalidToken):
+            exc_detail = {'token': [exc.args[0]['detail']]}
+        else:
+            exc_detail = {'error': [exc]}
         exc = ValidationError(detail=exc_detail)
 
     response = exception_handler(exc, context)
